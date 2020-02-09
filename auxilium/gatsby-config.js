9071231b2d69f986c3c8a823d011c14288ee92f6
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-02-09 03:57:13 +0000

    added so much stuff

--- auxilium/gatsby-config.js
+++ auxilium/gatsby-config.js
@@ -1,9 +1,47 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Auxilium`,
+    description: `kljfdalksjdfa.`
+  },
+  plugins: [
+    `gatsby-plugin-sass`, // For using SCSS to style project
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FFFFFF`,
+        display: `minimal-ui`,
+        // icon: `src/images/lego-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   }
+    // }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
--- auxilium/src/pages/index.js
+++ auxilium/src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react"
+import React from "react";
+// import { Link } from "gatsby";
+import Layout from "../components/layout";
+// import Footer from '../components/navigation/Footer';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => (
+  <> 
+  <Layout>
+    <h1>hello</h1>
+  </Layout>
+  </>
+)
+
+export default IndexPage;
