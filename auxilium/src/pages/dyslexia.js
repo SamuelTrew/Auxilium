00594a6599523053b1a40f002c4f996f9fc9b4d8
Samuel Trew <samuel.trew@outlook.com>
2020-02-09 03:14:15 +0000

    added comparison stuff

---
+++ auxilium/src/pages/dyslexia.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dyslexia = () => {
+   return (
+      <div className="App">
+         <header>
+
+         </header>
+      </div>
+   );
+};
+
+
+export default Dyslexia;
