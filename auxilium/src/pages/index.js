0777113dc8936f95b0d4cced18a5dbb28f87d53c
Matthew Pull <thematthewpull@gmail.com>
2020-02-09 03:58:32 +0000

    Initial slider

--- auxilium/src/pages/index.js
+++ auxilium/src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react";
 // import { Link } from "gatsby";
 import Layout from "../components/layout";
 // import Footer from '../components/navigation/Footer';
+import Slider from "../components/slider"
 
 const IndexPage = () => (
-  <> 
+  <>
   <Layout>
     <h1>hello</h1>
+    <Slider />
   </Layout>
   </>
 )
