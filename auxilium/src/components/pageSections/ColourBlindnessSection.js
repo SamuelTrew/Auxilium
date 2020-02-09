5d43104a51638e96fc27104f77b4aabf9ec317b2
Matthew Pull <thematthewpull@gmail.com>
2020-02-09 08:46:52 +0000

    Fix minor copy issues

--- auxilium/src/components/pageSections/ColourBlindnessSection.js
+++ auxilium/src/components/pageSections/ColourBlindnessSection.js
@@ -8,7 +8,7 @@ const ColourBlindness = () => {
                 <div className="container__row section-container">
                     <div className="container__col-sm-12">
                         <h4 className="is-black-text" style={{margin: '10px', marginLeft: '0'}}>
-                            Never Depend on Color Alone to Convey CriInformation
+                            Never Depend on Color Alone to Convey Critical Information
                         </h4>
                         <p className="is-black-text">
                         Visual elements need to be considered carefully to ensure that accessibility is not lost for users with visual impairments. Such users often access websites with special high-contrast web browsers, screen readers, or by replacing CSS styles with a set of rules designed to remove visual barriers. In order to create a site that provides the greatest degree of access to the visually impaired, consider the following tips.
@@ -17,7 +17,7 @@ const ColourBlindness = () => {
                             Something else? If you're looking for someone to join your team with new energy, ideas and skills for a placement or internship - let's talk! <span role="img" aria-label="nerd emoji">🤓</span>
                         </p>
                         
-                        <a id="dyslexia-link"className="button" href="/comparison" style={{cursor: "pointer"}}>
+                        <a id="dyslexia-link"className="button" href="/comparisons" style={{cursor: "pointer"}}>
                             <span>Click me</span>
                         </a>
                     </div>
--- auxilium/src/components/pageSections/IntroSection.js
+++ auxilium/src/components/pageSections/IntroSection.js
@@ -13,10 +13,10 @@ const IntroSection = () => {
                     </h2>
                         <br/>
                         <p className="is-black-text">
-                        The internet can be a hostile space for 15% of the world's population who experience some form of disability.
+                        The internet can be a hostile space for the 15% of the world's population who experience some form of disability.
                         </p>
                         <p className="is-black-text">
-                        Try navigating a website as someone who is visually impaired: Turn on voice command on your computer and go to Amazon's store. You'll quickly find out that those who rely on voice commands can't skip around and are doomed to listen to every notation about every page element before getting to the one piece of information they need. A 2016 Pew Research survey found that only 40% of disabled Americans feel comfortable using the internet compared to 80% of able-bodied users. In the UK, less than 30% are online. <span role="img" aria-label="nerd emoji">😢</span>
+                        Try navigating a website as someone who is visually impaired: turn on voice command on your computer and go to Amazon's store. You'll quickly find out that those who rely on voice commands can't skip around and are doomed to listen to every notation about every page element before getting to the one piece of information they need. A 2016 Pew Research survey found that only 40% of disabled Americans feel comfortable using the internet compared to 80% of able-bodied users. In the UK, less than 30% are online. <span role="img" aria-label="nerd emoji">😢</span>
                         </p>
                     </div>
                 </div>
