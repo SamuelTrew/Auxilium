6865412088445dae02a39c8a03178bf54c81c1d6
Samuel Trew <samuel.trew@outlook.com>
2020-02-09 03:57:26 +0000

    dyslexia start

--- auxilium/src/pages/dyslexia.js
+++ auxilium/src/pages/dyslexia.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import * as styles from "../styles/dyslexia.scss";
 
 const Dyslexia = () => {
    return (
       <div className="App">
          <header>
-
+            slfjjofnljfnsdljf
          </header>
+
+         <div style={styles.dys_bod}>
+            hellow there my dear freidn
+         </div>
       </div>
    );
 };
--- auxilium/src/pages/index.js
+++ auxilium/src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react"
+import Dyslexia from "./dyslexia";
 
-export default () => <div>Hello world!</div>
+export default () => <Dyslexia />
